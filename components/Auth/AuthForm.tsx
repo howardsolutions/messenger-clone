041a9e7810f1f58724bb734be7ee5f2eb966895a
thinhphungdev <phungthinh0946519356@gmail.com>
2024-05-02 10:39:01 +0700

    feat: Auth UI

--- components/Auth/AuthForm.tsx
+++ components/Auth/AuthForm.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
 import Input from '../Input';
 import Button from '../Button';
+import AuthSocialButton from './AuthSocialButton';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -40,7 +42,7 @@ function AuthForm() {
     }
   }
 
-  function socialAction(action: string) {
+  function signInWith(action: string) {
     setIsLoading(true);
 
     // NextAuth Social Signin
@@ -87,16 +89,61 @@ function AuthForm() {
 
         <div className='mt-6'>
           <div className='relative'>
-            <div className='absolute inset-0 flex items-center'>
-              <div className='w-full border-t border-gray-300' />
-              <div className='relative flex justify-center text-sm'>
-                <span className='bg-white px-2 text-gray-500'>
-                  Or Continue With
-                </span>
-              </div>
+            <div
+              className='
+                absolute
+                inset-0
+                flex
+                items-center
+              '
+            >
+              <div
+                className='
+                  w-full 
+                  border-t 
+                  border-gray-300'
+              />
+            </div>
+            <div
+              className='
+              relative 
+              flex 
+              justify-center 
+              text-sm
+            '
+            >
+              <span
+                className='
+                bg-white 
+                px-2 
+                text-gray-500'
+              >
+                Or continue with
+              </span>
             </div>
 
-            <div className='flex mt-6 gap-2'></div>
+            <div className='mt-6 flex gap-2'>
+              <AuthSocialButton
+                Icon={BsGithub}
+                onClick={() => signInWith('github')}
+              />
+              <AuthSocialButton
+                Icon={BsGoogle}
+                onClick={() => signInWith('google')}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className='flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500'>
+          <div>
+            {variant === 'LOGIN'
+              ? 'New to Messenger?'
+              : 'Already have an account?'}
+          </div>
+
+          <div onClick={toggleVariant} className='underline cursor-pointer'>
+            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
           </div>
         </div>
       </div>
--- components/Auth/AuthSocialButton.tsx
+++ components/Auth/AuthSocialButton.tsx
@@ -1,5 +1,38 @@
-function AuthSocialButton() {
-  return <div>AuthSocialButton</div>;
+'use client';
+
+import { IconType } from 'react-icons';
+
+interface AuthSocialButtonProps {
+  Icon: IconType;
+  onClick: () => void;
+}
+
+function AuthSocialButton({ Icon, onClick }: AuthSocialButtonProps) {
+  return (
+    <button
+      type='button'
+      onClick={onClick}
+      className='
+        inline-flex
+        cursor-pointer
+        w-full
+        justify-center
+        rounded-md
+        bg-white
+        px-4
+        py-2
+        text-gray-500
+        shadow-sm
+        ring-1
+        ring-inset
+        ring-gray-300
+        hover:bg-gray-50
+        focus:outline-offset-0
+      '
+    >
+      <Icon />
+    </button>
+  );
 }
 
 export default AuthSocialButton;
