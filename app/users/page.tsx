0ec0ee6b598033ba92c042eafc41fb7c5393254a
thinhphungdev <phungthinh0946519356@gmail.com>
2024-05-03 17:06:59 +0700

    feat: EmptyState component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,9 @@
-'use client';
-import { signOut } from 'next-auth/react';
+import EmptyState from '@/components/EmptyState';
 
 function UserPage() {
   return (
-    <div>
-      <button onClick={() => signOut()}>Logout</button>
+    <div className='hidden lg:block lg:pl-80 h-full'>
+      <EmptyState />
     </div>
   );
 }
