ca941a2a3cd1c33bc5aefe10458bf8e31c8ccf10
thinhphungdev <phungthinh0946519356@gmail.com>
2024-05-05 11:28:25 +0700

    feat: useOtherUser hook

---
+++ hooks/useOtherUser.ts
@@ -0,0 +1,15 @@
+import { FullConversationType } from "@/types";
+import { User } from "@prisma/client";
+import { useSession } from "next-auth/react";
+
+function useOtherUser(conversation: FullConversationType | { users: User[] }) {
+    const session = useSession();
+
+    const currentUserEmail = session?.data?.user?.email;
+
+    const otherUser = conversation.users.filter(user => user.email !== currentUserEmail);
+
+    return otherUser;
+}
+
+export default useOtherUser
